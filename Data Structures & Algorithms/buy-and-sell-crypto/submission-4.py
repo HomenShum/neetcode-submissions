29e6a7e52decc329676898b1e43852efa33fb01d
Homen Shum <77596827+HomenShum@users.noreply.github.com>
2026-05-18 23:51:12 -0700

    Add: buy-and-sell-crypto - submission-4

---
+++ Data Structures & Algorithms/buy-and-sell-crypto/submission-4.py
@@ -0,0 +1,11 @@
+class Solution:
+    def maxProfit(self, prices: List[int]) -> int:
+        # find lowest day potential and its pos
+        # then find the future highest potential after the pos
+        min_p = prices[0]
+        max_p = 0
+
+        for i in prices:
+            max_p = max(max_p, i - min_p)
+            min_p = min(min_p, i)
+        return max_p
